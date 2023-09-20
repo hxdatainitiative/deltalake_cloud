f3b43468bc4f471a7918a7f1dab7c7cdc979092c
Fernando Meza <fmeza@HEXACTA.COM>
2023-09-20 09:06:32 -0300

    minor fixes

--- terraform/glue.tf
+++ terraform/glue.tf
@@ -1,5 +1,6 @@
 module "test_job" {
-  source = "../modules/glue"
+  source     = "../modules/glue"
+  create_job = true
 
   name                = "${local.project_name}-write-job"
   description         = "Sample glue job to use"
@@ -12,37 +13,33 @@ module "test_job" {
   script_location     = "s3://${local.artifacts_bucket}/${module.glue_scripts.files["write_job.py"].key}"
   max_concurrent_runs = 1
   runtime_arguments = jsonencode({
-    "--datalake-formats":"delta",
-    "--continuous-log-logGroup": "/aws/glue/output",
-    "--enable-continuous-cloudwatch-log":"true",
-    "--enable-continuous-log-filter":"true",
-    "--additional-python-modules":"s3://${local.artifacts_bucket}/${module.glue_wheels.files["awswrangler-3.2.1-py3-none-any.whl"].key}",
-    "--short-env" : "dev"
+    "--datalake-formats" : "delta",
+    "--continuous-log-logGroup" : "/aws/glue/output",
+    "--enable-continuous-cloudwatch-log" : "true",
+    "--enable-continuous-log-filter" : "true",
+    "--additional-python-modules" : "s3://${local.artifacts_bucket}/${module.glue_wheels.files["awswrangler-3.2.1-py3-none-any.whl"].key}",
+    "--short-env" : "dev",
+    "--file_to_process" : "-"
     }
   )
 }
 
 module "merge_job" {
-  source = "../modules/glue"
+  source     = "../modules/glue"
+  create_job = true
 
   name                = "${local.project_name}-merge-job"
   description         = "Sample glue job to use"
-  glue_version        = "4.0"
-  timeout             = 600
-  worker_type         = "Standard"
-  number_of_workers   = 2
-  connections         = ""
-  python_version      = 3
   script_location     = "s3://${local.artifacts_bucket}/${module.glue_scripts.files["merge_job.py"].key}"
   max_concurrent_runs = 1
   runtime_arguments = jsonencode({
-    "--datalake-formats":"delta",
-    "--continuous-log-logGroup": "/aws/glue/output",
-    "--enable-continuous-cloudwatch-log":"true",
-    "--enable-continuous-log-filter":"true",
-    "--additional-python-modules":"s3://${local.artifacts_bucket}/${module.glue_wheels.files["awswrangler-3.2.1-py3-none-any.whl"].key}",
-    "--short-env" : "dev"
+    "--datalake-formats" : "delta",
+    "--continuous-log-logGroup" : "/aws/glue/output",
+    "--enable-continuous-cloudwatch-log" : "true",
+    "--enable-continuous-log-filter" : "true",
+    "--additional-python-modules" : "s3://${local.artifacts_bucket}/${module.glue_wheels.files["awswrangler-3.2.1-py3-none-any.whl"].key}",
+    "--short-env" : "dev",
+    "--file_to_process" : "-"
     }
   )
 }
-
